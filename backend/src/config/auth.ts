0e971e3bcdc1b01f8d78ee0cf5bbd078701c571f
rodffer <r.rodffer@gmail.com>
2020-04-17 09:40:47 -0300

    migrations-lists-services-users-auth-multer

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1e2aa44fd887caf239995ab3faa1f0be',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/database/migrations/1586907035593-CreateAppointments.ts
@@ -0,0 +1,71 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
+
+export default class CreateAppointments1586907035593
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+    await queryRunner.dropTable('appointments');
+  }
+}
