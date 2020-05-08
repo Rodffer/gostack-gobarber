465621089c6a273c643edd883f37fe96872a3187
Rayron Rodffer <r.rodffer@gmail.com>
2020-05-07 23:17:39 -0300

    frontend: Rotas autenticadas

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => <h1>Bem vindo ao Barber Shop!</h1>
+
+export default Dashboard;
---
+++ frontend/src/routes/Route.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { RouteProps as ReactRouteProps, Route as ReactRoute, Redirect} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface ReactProps extends ReactRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<ReactProps> = ({ isPrivate = false, component: Component, ...rest }) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactRoute {...rest} render={() => {
+      return isPrivate === !!user ? (
+        <Component />
+      ) : (
+        <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }}/>
+      )
+    }}
+    />
+  )
+};
+
+export default Route;
