203b23178588f7e15f3543796e5eef677e0cd3b5
Rayron Rodffer <r.rodffer@gmail.com>
2020-05-11 21:34:50 -0300

    mobile: StatusBar

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#112a4a" />
+    <View style={{ flex: 1, backgroundColor: '#112a4a' }} />
+  </>
+);
 
 export default App;
