a63f6d6b0c0aa5ea2ada339acc7390cc4efa9948
Rayron Rodffer <r.rodffer@gmail.com>
2020-05-11 21:58:58 -0300

    mobile: Routes

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,11 +1,18 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './Routes';
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#112a4a" />
-    <View style={{ flex: 1, backgroundColor: '#112a4a' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#112a4a' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
