dcd3f604befe5fb983225adad6fddffe949089fa
rodffer <r.rodffer@gmail.com>
2020-05-27 11:47:17 -0300

    backend: Controllers

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,23 +1,11 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionRouter = Router();
 
-sessionRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
+const sessionsController = new SessionsController();
 
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+sessionRouter.post('/', sessionsController.create);
 
 export default sessionRouter;
