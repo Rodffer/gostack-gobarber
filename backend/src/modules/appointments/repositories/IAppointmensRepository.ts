7fd50e4b3189b11632f4fef88fccd08c1185c4d4
Rayron Rodffer <r.rodffer@gmail.com>
2020-05-27 21:00:48 -0300

    testes: Teste unitario CreateAppointment

--- backend/src/modules/appointments/repositories/IAppointmensRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmensRepository.ts
@@ -3,5 +3,5 @@ import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findBydate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -21,7 +21,7 @@ class CreateAppointmentService {
   public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await this.appointmentsRepository.findBydate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
