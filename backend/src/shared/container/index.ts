0502972d6e8b1a7253156ce349f84e6534797158
Rayron Rodffer <r.rodffer@gmail.com>
2020-05-28 19:29:18 -0300

    testes: Testes de autenticacao

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 
+import '@modules/users/providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmensRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
